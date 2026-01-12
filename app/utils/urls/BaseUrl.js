0d9e3ab7d196d1fd33ea68fab7ebac438480c1c0
grash2024 <grash2024@gmail.com>
2026-01-12 11:18:40 +0530

    baseurl chnaged

--- app/utils/urls/BaseUrl.js
+++ app/utils/urls/BaseUrl.js
@@ -1,3 +1,3 @@
-//  const BASEURL = "https://mychits.online/api";
- const BASEURL = "http://localhost:3000/api";
+ const BASEURL = "https://mychits.online/api";
+//  const BASEURL = "http://localhost:3000/api";
  export default BASEURL;
